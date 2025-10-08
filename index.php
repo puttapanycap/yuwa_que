22ac8bfaca668d531fd12bf0f14366da0e6a0dc3
Dekwatdesign <75718734+dekwatdesign@users.noreply.github.com>
2025-10-08 11:47:20 +0700

    Service App installer

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#0d6efd" />
+    <link rel="manifest" href="./manifest.json" />
+    <meta name="mobile-web-app-capable" content="yes">
+
     <title>ระบบเรียกคิว - โรงพยาบาลยุวประสาทไวทโยปถัมภ์</title>
     
     <!-- Bootstrap 5 -->
@@ -516,6 +520,14 @@
             // submit หรือ fetch ไปค้นสินค้า
             }
         });
+    </script>
+
+    <script>
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('./sw.js').catch(console.error);
+            });
+        }
     </script>
 </body>
 </html>
