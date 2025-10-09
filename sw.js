6226fc748d61c24a186a378afc59990f215b830b
Dekwatdesign <75718734+dekwatdesign@users.noreply.github.com>
2025-10-09 13:09:04 +0700

    Revert "Service App installer"
    This reverts commit 22ac8bfaca668d531fd12bf0f14366da0e6a0dc3.

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => {
-  // สามารถใส่ pre-cache ได้ แต่ไม่ใส่ก็ยัง “ติดตั้งเป็นแอป” ได้
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  clients.claim();
-});
-// (ไม่ใส่ fetch ก็ได้; ถ้าต้องการ offline ค่อยทำ cache ตรงนี้)
