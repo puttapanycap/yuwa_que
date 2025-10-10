5edcf57bd7b3761dc485260065a3a8c5ee6b0218
Dekwatdesign <75718734+dekwatdesign@users.noreply.github.com>
2025-10-10 16:13:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ระบบเรียกคิว - โรงพยาบาลยุวประสาทไวทโยปถัมภ์</title>
-
     <meta name="theme-color" content="#690dfdff" />
     <link rel="manifest" href="./manifest.json" />
     <meta name="mobile-web-app-capable" content="yes">
-    
+    <title>ระบบเรียกคิว - โรงพยาบาลยุวประสาทไวทโยปถัมภ์</title>
+
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome 6 -->
